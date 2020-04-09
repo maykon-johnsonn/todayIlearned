f8c5994e5c24f5b74ff9aed3abbab44dbd39a665
Maykon Johnsonn <maykonjohnsonn@hotmail.com>
2020-04-08 23:55:25 -0300

    Sobre arrays

---
+++ array.js
@@ -0,0 +1,16 @@
+//literal
+var valores = ['maykon', 21, true];//no array posso adicionar todos os tipos de dados
+console.log(valores[0]); //forma de acessar o conteudo de um array
+
+//construtor
+var valor = new Array();
+
+valor[0] = "Johnsonn";
+valor[1] = 21;
+valor[2] = true;
+
+console.log(valor[0]);
+console.log(valor[1]);
+console.log(valor[2]);
+
+//Obs: diferente de algumas linguagens no JavaScript não é necessário pre definir o tamanho do array
